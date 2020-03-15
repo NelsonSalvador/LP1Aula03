e0d8bcda2ccb6438cfc059dc2f4894e911a96c10
NelsonSalvador <nelsonsalvadorzx@gmail.com>
2020-03-15 21:26:59 +0000

    Input do utilizador

---
+++ PrecorreString/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PrecorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            char c = Console.ReadLine();
+        }
+    }
+}
