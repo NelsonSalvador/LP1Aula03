3acf8d7f0a852e58a848f614d0ebdbb2eeec4487
NelsonSalvador <nelsonsalvadorzx@gmail.com>
2020-03-15 21:48:37 +0000

    Output

--- PrecorreString/Program.cs
+++ PrecorreString/Program.cs
@@ -7,7 +7,14 @@ namespace PrecorreString
         static void Main(string[] args)
         {
             string s = Console.ReadLine();
-            char c = Console.ReadLine();
+            string c = Console.ReadLine();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != c[0])
+                {
+                    Console.Write(s[i]);
+                }
+            }
         }
     }
 }
